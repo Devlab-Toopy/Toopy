7579e231dd4a828f0b643f458b676f2e79dbb90a
Luc MARIE <lucmarie.lm@gmail.com>
2019-02-03 17:31:38 +0100

    camera retry

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -90,61 +90,20 @@ async register(user: User, profile: Profile) {
 
   }
 
-getImage() {
-    const options: CameraOptions = {
+openGallery () {
+    let cameraOptions = {
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+        destinationType: Camera.DestinationType.FILE_URI,
         quality: 100,
-        destinationType: this.camera.DestinationType.FILE_URI,
-        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+        targetWidth: 1000,
+        targetHeight: 1000,
+        encodingType: Camera.EncodingType.JPEG,
+        correctOrientation: true
     }
 
-    this.camera.getPicture(options).then((imageData) => {
-        this.imageURI = imageData;
-    }, (err) => {
-        console.log(err);
-        this.presentToast(err);
-    });
+    Camera.getPicture(cameraOptions)
+        .then(file_uri => this.imageSrc = file_uri,
+            err => console.log(err));
 }
-
-uploadFile() {
-let loader = this.loadingCtrl.create({
-    content: "Uploading..."
-});
-loader.present();
-const fileTransfer: FileTransferObject = this.transfer.create();
-
-let options: FileUploadOptions = {
-    fileKey: 'ionicfile',
-    fileName: 'ionicfile',
-    chunkedMode: false,
-    mimeType: "image/jpeg",
-    headers: {}
-}
-
-fileTransfer.upload(this.imageURI, 'http://192.168.0.7:8080/api/uploadImage', options)
-    .then((data) => {
-        console.log(data+" Uploaded Successfully");
-        this.imageFileName = "http://192.168.0.7:8080/static/images/ionicfile.jpg"
-        loader.dismiss();
-        this.presentToast("Image uploaded successfully");
-    }, (err) => {
-        console.log(err);
-        loader.dismiss();
-        this.presentToast(err);
-    });
-}
-
-    presentToast(msg) {
-        let toast = this.toastCtrl.create({
-            message: msg,
-            duration: 3000,
-            position: 'bottom'
-        });
-
-        toast.onDidDismiss(() => {
-            console.log('Dismissed toast');
-        });
-
-        toast.present();
-    }
 
 }
